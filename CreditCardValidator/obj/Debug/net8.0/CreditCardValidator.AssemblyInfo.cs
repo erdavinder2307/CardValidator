0004ee2fdfd4ecdeaf9a52d4ecb825c40c3c8636
Davinder Pal <davinder@solidevelectrosoft.com>
2024-05-22 18:03:49 +0530

    Add new projects to solution

--- CreditCardValidator/obj/Debug/net8.0/CreditCardValidator.AssemblyInfo.cs
+++ CreditCardValidator/obj/Debug/net8.0/CreditCardValidator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CreditCardValidator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3a58599c2a2eb6ecf65456c1a5db5d8037e0fe0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CreditCardValidator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CreditCardValidator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
