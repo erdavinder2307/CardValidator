d860378e2d2152483ed7275f942c2b7eb4b78500
Davinder Pal <davinder@solidevelectrosoft.com>
2024-05-23 12:12:23 +0530

    chore: Add CreditCardValidator.Tests project to solution

---
+++ CreditCardValidator.Model/CardDetailsRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace CreditCardValidator.Model;
+
+public class CardDetailsRequest
+{
+    public required string CardNumber { get; set; }
+    public required string CardHolderName { get; set; }
+    public int ExpiryMonth { get; set; }
+    public int ExpiryYear { get; set; }
+    public int CVV { get; set; }
+}
---
+++ CreditCardValidator.Model/CardValidationResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace CreditCardValidator.Model;
+
+public class CardValidationResponse
+{
+    public bool IsValid { get; set; }
+    public required string CardNumber { get; set; }
+    public required string Message { get; set; }
+}
