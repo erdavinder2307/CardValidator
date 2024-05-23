1b26baa98a64e2baac0de55fc726ab99bdb6be81
Davinder Pal <davinder@solidevelectrosoft.com>
2024-05-23 12:09:40 +0530

    chore: Remove unnecessary cache and assembly info files, and add card validation endpoint

---
+++ CreditCardValidator.Tests/LuhnCardValidationServiceTest.cs
@@ -0,0 +1,33 @@
+﻿namespace CreditCardValidator.Tests;
+
+public class LuhnCardValidationServiceTest
+{
+    [Fact]
+    public void ValidateCard_WhenCardIsValid_ReturnsTrue()
+    {
+        // Arrange
+        var cardNumber = "4111111111111111";
+        var luhnCardValidationService = new LuhnCardValidationService();
+
+        // Act
+        var result = luhnCardValidationService.ValidateCard(cardNumber);
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void ValidateCard_WhenCardIsInvalid_ReturnsFalse()
+    {
+        // Arrange
+        var cardNumber = "4111111111111112";
+        var luhnCardValidationService = new LuhnCardValidationService();
+
+        // Act
+        var result = luhnCardValidationService.ValidateCard(cardNumber);
+
+        // Assert
+        Assert.False(result);
+    }
+
+}
---
+++ CreditCardValidator.Tests/VerhoeffCardValidationServiceTest.cs
@@ -0,0 +1,33 @@
+﻿namespace CreditCardValidator.Tests;
+
+public class VerhoeffCardValidationServiceTest
+{
+    [Fact]
+    public void ValidateCard_WhenCardIsValid_ReturnsTrue()
+    {
+        // Arrange
+        var cardNumber = "4166432103099989";
+        var verhoeffCardValidationService = new VerhoeffCardValidationService();
+
+        // Act
+        var result = verhoeffCardValidationService.ValidateCard(cardNumber);
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void ValidateCard_WhenCardIsInvalid_ReturnsFalse()
+    {
+        // Arrange
+        var cardNumber = "4111111111111112";
+        var verhoeffCardValidationService = new VerhoeffCardValidationService();
+
+        // Act
+        var result = verhoeffCardValidationService.ValidateCard(cardNumber);
+
+        // Assert
+        Assert.False(result);
+    }
+
+}
